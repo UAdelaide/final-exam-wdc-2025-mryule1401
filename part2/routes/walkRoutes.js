ae5fa72b982e5b726919b6c258edcda3a3c69137
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 14:16:06 +0000

    [automatic commit] 2025-06-20 14:16:06 UTC 122.150.236.30

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -4,15 +4,14 @@ const db = require('../models/db');
 
 // GET all walk requests (for walkers to view)
 router.get('/', async (req, res) => {
-  const { username } = req.session.user;
   try {
     const [rows] = await db.query(`
       SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'open' AND u.username = ?
-    `,[username]);
+      WHERE wr.status = 'open'
+    `);
     res.json(rows);
   } catch (error) {
     console.error('SQL Error:', error);
