45e426ceb0d341eda9803c0de66d9aa373df902a
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 13:30:05 +0000

    [automatic commit] 2025-06-20 13:30:04 UTC 122.150.236.30

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -48,7 +48,7 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-
+    req.session.user = rows[0]
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
