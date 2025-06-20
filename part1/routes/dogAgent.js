ed04abd49099f77fa97eb3506a3939e48bbfad86
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 02:05:32 +0000

    [automatic commit] 2025-06-20 02:05:32 UTC 122.150.236.30

--- part1/routes/dogAgent.js
+++ part1/routes/dogAgent.js
@@ -4,8 +4,8 @@ var db = require('../db');
 
 router.get('/dogs', async function(req, res) {
 const [rows] = await db.query(`
-    SELECT bl.BookID, b.Title, b.Author, u.Name AS SellerName, bl.SellerID
-    FROM BookListings bl
+    SELECT d.name,
+    FROM Dogs d
     JOIN BookInfo b ON bl.BookInfoID = b.BookInfoID
     JOIN Users u ON bl.SellerID = u.UserID
 `);
