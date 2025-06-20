98bdc8e29f6fcad9ad52b3bed6760689005f9006
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 01:57:08 +0000

    [automatic commit] 2025-06-20 01:57:08 UTC 122.150.236.152

--- part1/db.js
+++ part1/db.js
@@ -3,5 +3,6 @@ const mysql= require ('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password
+    password: 'mypassword',
+    database: 'Dogs'
 })
