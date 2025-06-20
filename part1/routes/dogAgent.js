b4b6a37980f7885dfffa7cb2edd56e282fd94432
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 02:16:45 +0000

    [automatic commit] 2025-06-20 02:16:45 UTC 122.150.236.218

--- part1/routes/dogAgent.js
+++ part1/routes/dogAgent.js
@@ -6,7 +6,7 @@ router.get('/dogs', async function (req, res) {
     const [rows] = await db.query(`
         SELECT d.name, d.size, u.username
         FROM Dogs d
-        JOIN Users u d.dog_id = u.user_id
+        JOIN Users u ON d.dog_id = u.user_id
     `);
     res.json(rows);
 });
