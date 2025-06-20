d7a1dd597bae782b39e5c33820b6876a5d088626
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 16:12:43 +0000

    [automatic commit] 2025-06-20 16:12:43 UTC 122.150.236.30

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -32,8 +32,7 @@ router.get('/me', async (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
-
-  res.json(req.session.user);
+  const { username } = 
 });
 
 // POST login (dummy version)
