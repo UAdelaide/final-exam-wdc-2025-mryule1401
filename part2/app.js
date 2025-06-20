9f0650b352f0405db993b20a5c7591c4b3d7483f
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 09:22:25 +0000

    [automatic commit] 2025-06-20 09:22:25 UTC 122.150.236.218

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
 
 const app = express();
 
