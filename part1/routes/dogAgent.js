e8457a3552e470001b2e23cfaa0b502463ff5b08
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 02:54:24 +0000

    [automatic commit] 2025-06-20 02:54:24 UTC 122.150.236.30

--- part1/routes/dogAgent.js
+++ part1/routes/dogAgent.js
@@ -11,7 +11,7 @@ router.get('/dogs', async function (req, res) {
     res.json(rows);
 });
 
-router.get('/walkrequest/open', async function (req, res) {
+router.get('/walkrequests/open', async function (req, res) {
     const [rows] = await db.query(`
         SELECT r.request_id, d.name AS dog_name, r.requested_time, r.duration_minutes, r.location, u.username
         FROM WalkRequests r
@@ -23,7 +23,7 @@ router.get('/walkrequest/open', async function (req, res) {
 });
 
 
-router.get('/', async function (req, res) {
+router.get('/walker', async function (req, res) {
     const [rows] = await db.query(`
         SELECT r.request_id, d.name AS dog_name, r.requested_time, r.duration_minutes, r.location, u.username
         FROM WalkRequests r
