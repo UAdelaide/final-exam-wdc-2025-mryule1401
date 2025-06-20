df8a02bb8739870821dd2f10bcae5320f7054428
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 03:05:36 +0000

    [automatic commit] 2025-06-20 03:05:36 UTC 122.150.236.98

--- part1/routes/dogAgent.js
+++ part1/routes/dogAgent.js
@@ -25,7 +25,7 @@ router.get('/walkrequests/open', async function (req, res) {
 
 router.get('/walkers/summary', async function (req, res) {
     const [rows] = await db.query(`
-        SELECT u.username, COUNT(wra.rating) AS total_ratings, AVG(wra.ratings) AS average_rating, COUNT(request_id) AS completed_walks
+        SELECT u.username, COUNT(wra.rating) AS total_ratings, AVG(wra.rating) AS average_rating, COUNT(request_id) AS completed_walks
         FROM Users u
         JOIN WalkApplications wa ON u.user_id= wa.walker_id AND wa.status='accepted'
         JOIN WalkRequests wre ON wa.request_id = wre.request_id AND wre.status='completed'
