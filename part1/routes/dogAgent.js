71655264ae1e379e7048aa00a173857bf3fda2b3
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 02:13:57 +0000

    [automatic commit] 2025-06-20 02:13:57 UTC 122.150.236.98

--- part1/routes/dogAgent.js
+++ part1/routes/dogAgent.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-router.get('/dogs', async function(req, res) {
+router.get('/dogs', async function (req, res) {
     const [rows] = await db.query(`
-    SELECT d.name, d.size, u.username
-    FROM Dogs d
-    JOIN Users u d.dog_id = u.user_id
-`);
-res.json(rows);
+        SELECT d.name, d.size, u.username
+        FROM Dogs d
+        JOIN Users u d.dog_id = u.user_id
+    `);
+    res.json(rows);
 });
 
-module.exports =router;
+module.exports = router;
