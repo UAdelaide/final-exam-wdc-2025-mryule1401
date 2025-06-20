0b990204f53fed7ded6261b1b18f7c1c0e3b2d43
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 02:38:11 +0000

    [automatic commit] 2025-06-20 02:38:11 UTC 122.150.236.30

--- part1/routes/dogAgent.js
+++ part1/routes/dogAgent.js
@@ -16,7 +16,7 @@ router.get('/walkrequest/open', async function (req, res) {
         SELECT r.request_id, d.dog_name AS name, r.requested_time, r.duration_minutes. r.location, u.username
         FROM WalkRequests r
         JOIN Dogs d ON r.dog_id = d.dog_id
-        JOIN Users u ON d.dog_id = u.user_id
+        JOIN Users u ON d.owner_id = u.user_id
         WHERE r.status ='open'
     `);
     res.json(rows);
