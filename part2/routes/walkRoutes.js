3305c02c30143353cb08fe0b3e5024ec50296fdc
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 14:11:48 +0000

    [automatic commit] 2025-06-20 14:11:47 UTC 122.150.236.218

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -22,6 +22,7 @@ router.get('/', async (req, res) => {
 // POST a new walk request (from owner)
 router.post('/', async (req, res) => {
   const { dog_id, requested_time, duration_minutes, location } = req.body;
+  const {username} = 
 
   try {
     const [result] = await db.query(`
