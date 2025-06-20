94a2e3cb202d43d557aa49a710eb14852ac037b0
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 16:09:06 +0000

    [automatic commit] 2025-06-20 16:09:06 UTC 122.150.236.98

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -73,7 +73,7 @@ router.get('/getdog', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    res.json(rows[0]);
+    res.json(rows.dog_id);
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
