398b3dba462b63fa15aac099c552de59d9d45b86
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 13:00:52 +0000

    [automatic commit] 2025-06-20 13:00:52 UTC 122.150.236.152

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -66,7 +66,7 @@ router.get('/getdog', async (req, res) => {
     const [rows] = await db.query(`
       SELECT d.name FROM Dogs d
       JOIN Users u ON d.owner_id = u.user_id
-      WHERE u.username = ? AND u.password_hash = ?
+      WHERE u.username = ?
     `, [username]);
 
     if (rows.length === 0) {
