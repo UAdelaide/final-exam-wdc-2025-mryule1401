d142e02957781cc73fa00ce3dcf34e2b3b8e7a98
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 02:08:21 +0000

    [automatic commit] 2025-06-20 02:08:21 UTC 122.150.236.98

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dogRouter = require('./routes/dogAgent')
+var dogRouter = require('./routes/dogAgent');
 
 var app = express();
 
@@ -17,6 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api')
+app.use('/api',dogRouter)
 
 module.exports = app;
