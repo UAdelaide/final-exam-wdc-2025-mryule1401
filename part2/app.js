c860c84e26bd3654a9f129c8322359c6e39d657c
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 09:22:46 +0000

    [automatic commit] 2025-06-20 09:22:46 UTC 122.150.236.30

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-// app.use(cookieParser());
+app.use(cookieParser());
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
