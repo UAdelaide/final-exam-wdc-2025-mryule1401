3e4be72136bcdfc05c6ef87310c239a47b828059
Mryule1401 <lehuy011406@gmail.com>
2025-06-20 14:12:10 +0000

    [automatic commit] 2025-06-20 14:12:09 UTC 122.150.236.30

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -22,12 +22,13 @@ router.get('/', async (req, res) => {
 // POST a new walk request (from owner)
 router.post('/', async (req, res) => {
   const { dog_id, requested_time, duration_minutes, location } = req.body;
-  const {username} = 
+  const {username} = req.session.user;
 
   try {
     const [result] = await db.query(`
       INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location)
       VALUES (?, ?, ?, ?)
+      WHERE
     `, [dog_id, requested_time, duration_minutes, location]);
 
     res.status(201).json({ message: 'Walk request created', request_id: result.insertId });
